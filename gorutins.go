c85faf9c4cad91c24b457f7534286bc990521b18
Влад <wfalcon@mail.ru>
2022-03-03 08:40:57 +0300

    Example works gorutines, chanel and wait group
    пример работы горутин, каналов и waitgroup

--- gorutins.go
+++ gorutins.go
@@ -8,28 +8,28 @@ import (
 	"time"
 )
 
-func gofer(i int, start chan int, wg *sync.WaitGroup, gc int) {
+func gofer(i int, ch chan int, wg *sync.WaitGroup, gc int) {
 	defer wg.Done()
 	sl := time.Duration(rand.Intn(gc))
 	time.Sleep(sl * time.Second)
 	fmt.Printf("Я горутина № %v ,работала %v sec \n", i+1, int(sl))
-	start <- i
+	ch <- i
 
 }
 
 func main() {
 	gc := 100
-	var start = make(chan int, gc)
+	var ch = make(chan int, gc)
 	wg := &sync.WaitGroup{}
 
 	for i := 0; i < gc; i++ {
 		wg.Add(1)
-		go gofer(i, start, wg, gc)
+		go gofer(i, ch, wg, gc)
 
 	}
 	wg.Wait()
-	close(start)
-	for i := range start {
+	close(ch)
+	for i := range ch {
 
 		fmt.Printf("Горутина № %v закончила работу \n", i+1)
 
